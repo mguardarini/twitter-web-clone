df2131f60483ccfe25eca4827a967a900b567001
Mauro Guardarini <mauro.guardarini@gmail.com>
2019-10-29 00:02:27 -0300

    inicio da mudança da tela de login

--- src/screens/Login/login.js
+++ src/screens/Login/login.js
@@ -3,11 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Footer from '../../components/Footer/footer'
 import img_login from '../../Images/login.png'
+import img_boasvindas from '../../Images/twitter-frase.PNG'
+
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import clsx from 'clsx';
 
 export default function FullWidthGrid() {
     const classes = useStyles();
@@ -19,25 +20,33 @@ export default function FullWidthGrid() {
              <img width='100%' src={img_login}></img>
           </Grid>
           <Grid item sm={6} xs={12}>
+             <Grid className={classes.grid_text}>
               <TextField
-                      id="outlined-dense"
-                      label="Dense"
-                      className={classes.dense}
-                      margin="dense"
-                      variant="outlined"
-                    />
-                    <TextField
                     id="outlined-dense"
-                    label="Dense"
-                    className={classes.dense}
+                    className={classes.textfield}
+                    label="Nome, "
                     margin="dense"
                     variant="outlined"
                   />
-                  <Button variant="outlined" color="primary" className={classes.fab}>
-                    Entrar
+                    <TextField
+                    id="outlined-dense"
+                    className={classes.textfield}
+                    margin="dense"
+                    variant="outlined"
+                  />
+                  <Button variant="outlined" text="Entrar" color="primary" className={classes.fab}>
+                      Entrar
                   </Button>
-
-
+              </Grid>
+              <Grid className={classes.grid_image}>
+                  <img width='80%' src={img_boasvindas}></img>
+                  <Button variant="outlined" text="Entrar" color="primary" className={classes.fab2}>
+                      Inscreva-se
+                  </Button>
+                  <Button variant="outlined" text="Entrar" color="primary" className={classes.fab2}>
+                      Entrar
+                  </Button>
+              </Grid>
           </Grid>       
         </Grid>
         <Footer/>
@@ -51,13 +60,50 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
     color: '#e6ecf0',
   },
+  grid_image: {
+    alignContent: 'center',
+    marginLeft:'24%',
+    marginTop:'80px',
+    marginBottom:'80px',
+    marginRight:'24%',
+    align: 'center',
+  },
+  grid_text: {
+    alignContent: 'center',
+    marginLeft:'15%',
+    marginTop:'3%'
+  },
   fab: {
     margin: theme.spacing(1),
-    marginTop:'20px'
+    marginTop:'20px',
+    width:'70px',
+    height:'50px',
+    fontFamily:'Segoe UI',
+    borderRadius:'60px',
+    textTransform: 'none',
   },
-  dense: {
+  fab2: {
     margin: theme.spacing(1),
-    marginTop:'20px'
+    marginTop:'20px',
+    width:'100%',
+    height:'30px',
+    fontFamily:'Segoe UI',
+    borderRadius:'60px',
+    textTransform: 'none',
+    paddingBottom: '20px',
+  },
+  textfield: {
+    margin: theme.spacing(1),
+    marginTop:'20px',
+    width:'200px',
+    height:'100px',
+    fontSize:'11px',
+    fontStyle: 'normal',
+    fontFamily:'Segoe UI',
 
   },
+  container: {
+    marginTop:'30px',
+    marginLeft:'60px'
+  }
 }));
