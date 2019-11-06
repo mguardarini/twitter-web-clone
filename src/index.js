f4d467f38a31d69ffc95221df935d6e138852a86
Mauro Guardarini <mauro.guardarini@gmail.com>
2019-11-06 00:40:30 -0300

    include first animation with pure scss

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './screens/Login/login';
+import Dashboard from './screens/Dashboard/dashboard';
+
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
-ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<Dashboard />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
